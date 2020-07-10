2ca7ef41409818a4de8a278fc5413914bb72274b
Rafael Lua <50277486+RafaelLua13@users.noreply.github.com>
2020-07-09 21:46:37 -0300

    Create Intercalar Vetores.py

---
+++ Intercalar Vetores.py
@@ -0,0 +1,67 @@
+# Intercalar dois vetores sem comandos lista
+
+# Nome do projeto: Intercalar Vetores
+# Linguagem: Python
+# Utilizações: Variáveis, Repetições e Vetores
+# Autor: Rafael Lua - rafaellua13
+
+A = [1,3,6,7,8,9]
+B = [2,4,5,10]
+
+a = 0
+b = 0
+C = [0] * (len(A) + len((B))) 
+
+for x in range(len(C)):
+  if A[a] < B[b]:
+    C[x] = A[a]
+
+    if a + 1 < len(A):
+      a += 1
+
+    else: 
+
+      A[a] = B[b] + A[a] 
+
+  else:
+    C[x] = B[b]
+
+    if b + 1 < len(B):
+      b += 1
+
+    else: 
+      B[b] = A[a] + B[b]
+
+print(C)
+
+# Usando comandos Listas
+
+
+# A = [1,3,6,7,8,9]
+# B = [2,4,5,10]
+
+# a = 0
+# b = 0
+# C = []
+
+# for x in range(len(A) + len((B))):
+#   if A[a] < B[b]:
+#     C.append(A[a])
+
+#     if a + 1 < len(A):
+#       a += 1
+
+#     else: 
+
+#       A[a] = B[b] + A[a] 
+
+#   else:
+#     C.append(B[b])
+
+#     if b + 1 < len(B):
+#       b += 1
+
+#     else: 
+#       B[b] = A[a] + B[b]
+
+# print(C)
